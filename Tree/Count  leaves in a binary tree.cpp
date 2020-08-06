6f319f87270802686ad730250b782d8d6e7c78ac
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 17:41:09 +0530

    Create Count  leaves in a binary tree.cpp

---
+++ Tree/Count  leaves in a binary tree.cpp
@@ -0,0 +1,51 @@
+/*
+
+Ques :- Count number of leaves in a binary tree
+
+Method 1:- in O(n) space using queue
+
+*/
+
+int countLeaves(Node* root)
+{
+  // Your code here
+  queue<Node*> q1;
+  q1.push(root);
+  
+  int cLeaves=0;
+  
+  while(!q1.empty())
+  {
+      if(q1.front()->left == NULL && q1.front()->right == NULL)
+      ++cLeaves;
+      else 
+      {
+          if(q1.front()->left != NULL)
+          q1.push(q1.front()->left);
+          
+          if(q1.front()->right != NULL)
+          q1.push(q1.front()->right);
+      }
+      
+      q1.pop();
+  }
+  
+  return cLeaves;
+}
+
+
+*/
+
+Method 2:- (using recursion)
+
+*/
+
+int countLeaves(Node* root)
+{
+  // Your code here
+  if(root == NULL) return 0;
+  else if(root->left == NULL && root->right == NULL)
+  return 1;
+  else
+  return(countLeaves(root->left) + countLeaves(root->right));
+}
