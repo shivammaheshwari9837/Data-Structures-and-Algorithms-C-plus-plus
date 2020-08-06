69a47ca95281d9798ddebc522029eefe63fd9e4d
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-07 02:55:15 +0530

    Update Check if subtree.cpp

--- Tree/Check if subtree.cpp
+++ Tree/Check if subtree.cpp
@@ -9,27 +9,30 @@ Concept:- 1.)Traversing each node of T subtree and checking S is a subtree of T 
 
      bool isSame(TreeNode* s,TreeNode *t)
     {
-        if(s == NULL || t == NULL)
+        if(s == NULL || t == NULL) 
         {
-            return (s == NULL && t == NULL);
+            return (s == NULL && t == NULL);  // both must be null or not null
         }
-        else if(s->val == t->val)
-            return isSame(s->left,t->left) && isSame(s->right,t->right);
+        else if(s->val == t->val) // if elemnt matches, continue search
+            return isSame(s->left,t->left) && isSame(s->right,t->right); 
         else
             return false;
     }
 
     bool isSubtree(TreeNode* s, TreeNode* t) {
-        if(s == NULL)
+        if(s == NULL)  //if s is null
             return false;
-        else if(isSame(s,t))
+        else if(isSame(s,t)) // checking s (as a root) and t (as a root)
             return true;
         else
-            return isSubtree(s->left,t) || isSubtree(s->right,t);
+            return isSubtree(s->left,t) || isSubtree(s->right,t); //else  moving towards different level it. left and right 
         
     }
     
     
     /*
+    
+    Method 2:- O(n) time
+    Concept:- Rabin Karp(at end)
     
     */
