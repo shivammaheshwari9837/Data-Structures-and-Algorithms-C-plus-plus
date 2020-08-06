075a032db1c2642bbdb60249b1d792739e5217a8
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 14:58:04 +0530

    Update Find if s2 is a rotated version of the string s1.cpp

--- String/Find if s2 is a rotated version of the string s1.cpp
+++ String/Find if s2 is a rotated version of the string s1.cpp
@@ -9,3 +9,23 @@ Concept:-Step 1: Create a temp string and store concatenation of str1 to str1 in
 
 */
 
+
+void work()
+{
+	string s1,s2;
+	cin>>s1>>s2;
+	
+	if(s1.length() != s2.length())
+	{
+	    cout<<0;
+	    return;
+	}
+	
+	s1+=s1;
+	
+	if(s1.find(s2) != string::npos)
+	cout<<1;
+	else
+	cout<<0;
+}
+
