d60921d9028077683797af7114be53a5cd8676f0
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 12:38:40 +0530

    Create implement stack using heap or priority queue(min heap).cpp

---
+++ Stack/implement stack using heap or priority queue(min heap).cpp
@@ -0,0 +1,11 @@
+/*
+
+Ques :- How to implement stack using heap or priority queue(min heap)
+
+Concep:- 1. Just make min heap of pair---->priority_queue<pii,vector<pii>,greater<pii> > min;
+		where pii-> pair<int,int> 
+	2. initialize an counter that saves the entering time of an element
+	3. make that counter as first 
+
+
+*/
