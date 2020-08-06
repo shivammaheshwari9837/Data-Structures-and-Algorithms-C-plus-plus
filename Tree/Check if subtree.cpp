46ad8d29bb733c2ac3a2fa083b0e394eb2a85cac
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-07 02:49:08 +0530

    Create Check if subtree.cpp

---
+++ Tree/Check if subtree.cpp
@@ -0,0 +1,35 @@
+/*
+
+Ques :-Given two binary trees with head reference as T and S having at most N nodes. The task is to check if S is present as subtree in T.
+
+Method 1:- O(s_nodes * t_nodes) or O(n^2) , space - O(min_nodes(s,t)
+Concept:- 1.)Traversing each node of T subtree and checking S is a subtree of T or not
+
+*/
+
+     bool isSame(TreeNode* s,TreeNode *t)
+    {
+        if(s == NULL || t == NULL)
+        {
+            return (s == NULL && t == NULL);
+        }
+        else if(s->val == t->val)
+            return isSame(s->left,t->left) && isSame(s->right,t->right);
+        else
+            return false;
+    }
+
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        if(s == NULL)
+            return false;
+        else if(isSame(s,t))
+            return true;
+        else
+            return isSubtree(s->left,t) || isSubtree(s->right,t);
+        
+    }
+    
+    
+    /*
+    
+    */
