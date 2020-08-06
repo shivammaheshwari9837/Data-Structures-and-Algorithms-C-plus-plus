7c820c6dd214f9b8a4fd587d7c262fe8ed391725
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 14:57:30 +0530

    Create Find if s2 is a rotated version of the string s1.cpp

---
+++ String/Find if s2 is a rotated version of the string s1.cpp
@@ -0,0 +1,11 @@
+/*
+
+Ques :- Given strings s1 and s2, you need to find if s2 is a rotated version of the string s1. The strings are lowercase.
+
+Concept:-Step 1: Create a temp string and store concatenation of str1 to str1 in temp. 
+             temp = str1.str1
+	Step 2: If str2 is a substring of temp then str1 and str2 are 
+            rotations of each other using KMP/Rabin Carp/Z algorithm.. or inbuilt function find can be 	used.
+
+*/
+
