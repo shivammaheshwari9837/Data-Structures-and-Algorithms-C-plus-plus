67be8a8d6c58e3efad7e9cded115e8a2581a5666
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 14:02:16 +0530

    Create Merge two non-decreasing linked list into one decreasing.cpp

---
+++ Linked List/Merge two non-decreasing linked list into one decreasing.cpp
@@ -0,0 +1,13 @@
+/*
+
+Ques :- Given two linked lists of size N and M, which are sorted in non-decreasing order. The task is to merge them in such a way that the resulting list is in decreasing order.
+
+Concept:- 
+MEthod1:-
+	1) Reverse the first and second linked list using 3 pointer approach
+	2) Than do 2-way merging using pointers.
+Method2:- 
+	1)First merge two sorted linked list
+	2) Than simply revrse it...
+  
+  */
