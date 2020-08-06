4a999e9159b4167359a9e9bfb392eb4435b84f80
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-06 11:44:37 +0530

    Create Search an elemnet in a bitonic array.cpp

---
+++ Binary Search/Search an elemnet in a bitonic array.cpp
@@ -0,0 +1,10 @@
+/*
+
+Ques :- Search an elemnet in a bitonic array
+
+Theory :- 1. Find the peak element
+	2. The peak element divides the array into two parts:- incresing and decreasing
+	array
+	3. Apply binary search two times..
+
+*/
