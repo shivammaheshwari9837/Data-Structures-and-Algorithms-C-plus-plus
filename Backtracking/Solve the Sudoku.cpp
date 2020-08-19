c99c9683f6734a7708167b2555c9c6d896f6be2a
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-19 09:19:14 +0530

    Update Solve the Sudoku.cpp

--- Backtracking/Solve the Sudoku.cpp
+++ Backtracking/Solve the Sudoku.cpp
@@ -9,7 +9,7 @@ Concept:- 1)Like all other Backtracking problems, we can solve Sudoku by one by 
           3)After checking for safety, we assign the number, and recursively check whether this assignment leads to a solution or not. If the assignment doesn’t 
           lead to a solution, then we try next number for the current empty cell. And if none of the number (1 to 9) leads to a solution, we return false.
           
- Time - O(9*9*(9^n)*9) 
+ Time - O(9*9*(9^9)*9) 
 
 */
 
