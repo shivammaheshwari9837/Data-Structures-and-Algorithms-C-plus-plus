e6c657b6a176750f125514ac508154730c73887c
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-08-20 13:46:21 +0530

    Update Hamilton Path.cpp

--- Backtracking/Hamilton Path.cpp
+++ Backtracking/Hamilton Path.cpp
@@ -81,6 +81,84 @@ int main()
 
 /*
 
-Method 2:- 
+Method 2:- Using the DFS method
+	    1)apply depth first search starting from every vertex v and do labeling of all the vertices.
+	    2)All the vertices are labelled as either "IN STACK" or "NOT IN STACK". A vertex is labelled "IN STACK" if it is visited but some of its adjacent vertices 
+	    are not yet visited and is labelled "NOT IN STACK" if it is not visited.
+	    3)If at any instant the number of vertices with label "IN STACK" is equal to the total number of vertices in the graph then a Hamiltonian Path exists 
+	    in the grap
+
+Time - Worst case is O(!n)
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+bool dfs(int vertex,bool inStack[],int n,vector<int> gra[],int coun)
+{
+    if(coun == n) //if total nodes in path equal to n,than path exists
+    return true;
+    
+    for(int i=0;i<gra[vertex].size();++i) 
+    {
+        int child = gra[vertex][i];
+        
+        if(inStack[child] != true) //if not in path and is a connected node with current vertex
+        {
+            inStack[child] = true; //make it in the path
+            
+            if(dfs(child,inStack,n,gra,coun+1))
+            return true;
+            
+            inStack[child] = false; //remove it from the path,so next path can utilize it ,while backtracking
+        }
+    }
+    
+    return false;
+}
+
+void work()
+{
+    int n,m,x,y;
+    cin>>n>>m;
+    
+    vector<int> gra[n+1];
+    
+    for(int i=0;i<m;++i)
+    {
+        cin>>x>>y;
+        gra[x].push_back(y);
+        gra[y].push_back(x);
+    }
+    
+    bool inStack[n+1] = {false};
+    bool ans = true;
+    
+    for(int i=1;i<=n;++i)
+    {
+        inStack[i] = true; // push the node to the path
+        ans = dfs(i,inStack,n,gra,1);
+        if(ans)
+        {
+            cout<<1;
+            return;
+        }
+        inStack[i] = false;
+    }
+    
+    cout<<0;
+}
+
+int main()
+ {
+	ll test;
+	cin>>test;
+	for(int i=0;i<test;++i)
+	{
+	    work();
+	    cout<<"\n";
+	}
+	return 0;
+}
