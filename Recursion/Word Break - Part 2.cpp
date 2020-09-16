97b1cc0fe912b57c72a57a3eeacc467a5f1014a7
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-09-16 19:26:59 +0530

    Create Word Break - Part 2.cpp

---
+++ Recursion/Word Break - Part 2.cpp
@@ -0,0 +1,120 @@
+/*
+
+Ques:- Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+        Return all such possible sentences.
+        
+Concept:- 1) First try to see this question ie. Word Break 1(gfg or Leetcode)
+          Code Link:-    https://github.com/shivammaheshwari9837/DS-and-Algorithms/blob/master/Dynamic%20Programming/Word%20Break.cpp
+          
+          2)than we only have to print all valid possibilty in acending sorted order
+          
+        
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<string>  ans;
+
+bool check(int st,int bk,string &str,vector<string> &dict)
+{
+	string temp;
+	
+	for(int i=st;i<bk;++i)
+	temp.push_back(str[i]);
+	
+	for(int i=0;i<dict.size();++i)
+	{
+		if(temp.compare(dict[i]) == 0)
+		return true;
+	}
+	return false;
+}
+
+void segment(int st,int bk,string &str,vector<string> &dict,vector<string> temp)
+{
+	//base...
+	if(bk == str.size())
+	{
+		if(check(st,bk,str,dict))
+		{
+			string p;
+			for(int i=st;i<bk;++i)
+			{
+				p.push_back(str[i]);
+			}
+			temp.push_back(p);
+			
+			string x;
+			
+			for(int i=0;i<temp.size();++i)
+			{
+			    for(int j=0;j<temp[i].size();++j)
+			    x.push_back(temp[i][j]);
+			    
+			    if(i!=temp.size()-1)
+			    x.push_back(' ');
+			}
+			
+			ans.push_back(x);
+		}
+		return;
+	}
+	
+	//not break it..
+	segment(st,bk+1,str,dict,temp);
+	
+	if(check(st,bk,str,dict))
+	{
+		string p;
+		for(int i=st;i<bk;++i)
+		{
+			p.push_back(str[i]);
+		}
+		
+		temp.push_back(p);
+		segment(bk,bk+1,str,dict,temp);
+	}
+}
+
+void work()
+{
+	int n;
+	cin>>n;
+	
+	vector<string> dict(n);
+	
+	for(int i=0;i<n;++i)
+	cin>>dict[i];
+	
+	string str;
+	cin>>str;
+	
+	vector<string> temp;
+	
+	ans.clear();
+	
+	segment(0,1,str,dict,temp);
+	
+	sort(ans.begin(),ans.end());
+	
+	for(int i=0;i<ans.size();++i)
+	{
+	    cout<<"("<<ans[i]<<")";
+	}
+}
+
+int main()
+{
+	int test;
+	cin>>test;
+	
+	while(test--)
+	{
+		work();
+		cout<<"\n";
+	}
+	return 0;
+}
