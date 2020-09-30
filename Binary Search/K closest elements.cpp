aee078acc0ef39324480f54f77c27e44f7346219
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2020-09-30 18:02:30 +0530

    Update K closest elements.cpp

--- Binary Search/K closest elements.cpp
+++ Binary Search/K closest elements.cpp
@@ -1,12 +1,23 @@
 /*
 
-Ques:- 
+Ques:- Given a sorted array, arr[] of N integers, and a value X. Find the K closest elements to X in arr[].
+	Keep the following points in mind:
 
+	If X is present in the array, then it need not be considered.
+	If there are two elements with the same difference with X, the greater element is given priority.
+	If sufficient elements are not present on the right side then take elements from left and vice versa.
+
+Concept:- 1)we maintain two indices, which will maintain the nearest indices in which the X element found
+	  2)Since array is sorted, than we will apply binary search in O(log n) time
+	  3)After that we maintain two pointers (rIndex,lIndex), which will keep the track of closest element
+	
+Time - O(logn + K)
+Space - O(1)
 */
 
 vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
         // code here
-        int rIndex = arr.size(),lIndex = -1;
+        int rIndex = arr.size(),lIndex = -1; //we try to find the indices in which x can lie in O(log n )
         
         int low = 0,high = arr.size()-1;
         vector<int> ans;
@@ -32,19 +43,17 @@ vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
                 high = mid-1;
             }
         }
-        
-//        cout<<lIndex<<" "<<rIndex<<"\n";
 		
-		int left = lIndex,right = rIndex;
+		int left = lIndex,right = rIndex; //two pointers used as maintained above
 		
 		while(left>=0 && right<n && k > 0)
 		{
-			if(arr[left] == x)
+			if(arr[left] == x) //if element is same as X,discard it
 			{
 				--left;
 				continue;
 			}
-			if(arr[right] == x)
+			if(arr[right] == x) //if element is same as X,discard it
 			{
 				++right;
 				continue;
@@ -67,21 +76,19 @@ vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
 			--k;
 		}
 		
-		while(k>0 && left < 0)
+		while(k>0 && left < 0) //if some k is left,and left part is empty,than include only right part
 		{
 			ans.push_back(arr[right]);
 			++right;
 			--k;
 		}
 		
-		while(k>0 && right >=n)
+		while(k>0 && right >=n) //if some k is left,and right part becomes empty,than only includes left part elements
 		{
 			ans.push_back(arr[left]);
 			--left;
 			--k;
 		}
-		
-//		sort(ans.begin(),ans.end());
         
         return ans;
     }
